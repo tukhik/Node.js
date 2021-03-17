f2da0f3b118367dd20105ca1732f1d7c5a8f1332
Tukhik <tukhik_k@mail.ru>
2021-03-17 23:29:14 +0400

    update homework 17

--- homework3.js
+++ homework3.js
@@ -3,6 +3,7 @@
 //3
 const os = require('os');
 const fs = require("fs"); 
+
 const str = String(os.cpus().length);
 const num = String(Math.round((Math.random() *89999999 + 10000000)));
 fs.writeFile(num + '.txt', str, ()=>{
