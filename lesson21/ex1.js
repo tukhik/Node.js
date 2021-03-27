c065dd67a6971625d77675924122208d601d0b62
Tukhik <tukhik_k@mail.ru>
2021-03-27 11:59:49 +0400

    create new branch

--- lesson21/ex1.js
+++
@@ -1,9 +0,0 @@
-//1.Գրել ծրագիր, որը կստուգի , եթե req.url /sunny է, ապա console-ում տպի Yes.
-
-let http =  require('http');
-
-http.createServer( function (request, response) {  
-   if(request.url == '/sunny'){
-   response.end(console.log('YES'))
-	}
-}).listen(3000);
