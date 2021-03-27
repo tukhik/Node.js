e76579d4fa048df4e2afa59d23cb183685f60fb0
Tukhik <tukhik_k@mail.ru>
2021-03-27 11:56:36 +0400

    update homework

--- lesson21/ex3.js
+++ lesson21/ex3.js
@@ -1,30 +1,23 @@
-//3. Server-ում ունենք users.json ֆայլ, որը զանգված է կազմված "fisrt_name", "last_name", "email", "age" 
-//դաշտեր պարունակող օբյեկտներից ։ Ստեղծել սերվեր, որին հարցում ուղարկելիս եթե կա query-ի մեջ filter դաշտը , 
-//վերադարձնել users.json-ից զանգված միայն այն օբյեկներից , 
-//որոնց "fisrt_name" կամ "last_name" պարունակում է filter-ի արժեքը։
-
+//3. Server-ում ունենք users.json ֆայլ,
+// որը զանգված է կազմված "fisrt_name", "last_name", "email", "age" դաշտեր պարունակող օբյեկտներից ։
+// Ստեղծել սերվեր, որին հարցում ուղարկելիս եթե կա query-ի մեջ filter դաշտը , վերադարձնել users.json-ից զանգված միայն այն օբյեկներից ,
+// որոնց "fisrt_name" կամ "last_name" պարունակում է filter-ի արժեքը։
 const http = require('http');
-const fs = require('fs');
+const users = require('./users.json');
 const url = require('url');
 
-http.createServer((req, res)=>{
-	let arr = [];
-	
-
-
-	res.setHeader('Content-Type', 'application/json');
-  	let f = fs.readFile('users.json', function (err, data) {  
-	   if (err) {  
-	       return console.error(err);  
-	   }  
-   	let str=data.toString()
-	res.end(str)
-	//res.statusCode  = 200;
-	}); 
-	/*if(req.query.filter == f.fisrt_name || req.query.filter == f.last_name ){
-		arr.push(f)
-	} */
-	console.log(req.query.filter)
-    //res.end(JSON.stringify({user: "Sargis"}));
-
-}).listen(3000)
+http.createServer((req, res) => {
+    const reqUrl = new URL(req.url, 'http://localhost:3000');
+    const filter = reqUrl.searchParams.get('filter');
+    console.log(filter);
+    res.writeHead(200, {
+        'ContentType': 'application/json'
+    })
+    if (filter){
+        res.end(JSON.stringify(users.filter(user=> {
+            return user['first_name'].includes(filter) || user['last_name'].includes(filter);
+        })));
+    } else {
+        res.end(JSON.stringify(users));
+    }
+}).listen(3000);
